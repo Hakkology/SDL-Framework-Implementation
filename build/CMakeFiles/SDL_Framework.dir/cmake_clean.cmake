a5071293bd29285cc1c214b1601887af0389bfce
Hakkology <yildizhakan88@gmail.com>
2023-07-01 19:30:07 +0300

    SDL-Framework

---
+++ build/CMakeFiles/SDL_Framework.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL_Framework.dir/src/main.cpp.o"
+  "CMakeFiles/SDL_Framework.dir/src/main.cpp.o.d"
+  "SDL_Framework"
+  "SDL_Framework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL_Framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SDL_Framework.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SDL_Framework.
+# This may be replaced when dependencies are built.
